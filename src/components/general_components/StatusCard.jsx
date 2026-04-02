5a219a111012c8d86ae0c116ed6cf94228afe3ed
cheeseball <cheeseballbusinessemail@gmail.com>
2026-04-02 12:05:18 +0800

    Added four temporary status cards Adjusted color values of texts in dashboard

--- src/components/general_components/StatusCard.jsx
+++ src/components/general_components/StatusCard.jsx
@@ -3,21 +3,22 @@ import { CircleQuestionMark } from 'lucide-react';
 const StatusCard = ({
     title, 
     mainValue = "0", 
-    subText = "no label",
+    subText,
     Icon = CircleQuestionMark, 
     color = "text-custom-black",
     secondValue,
     thirdValue,
+    secondSubText,
 }) => {
 
     return (
-        <div className="border border-b-slate-100 rounded-2xl p-6 w-full h-full shadow-sm mr-4 transition duration-300 hover:scale-110">
+        <div className="border border-custom-gray rounded-2xl p-6 w-max-50 h-full shadow-sm mr-4 transition duration-300 hover:scale-105">
             <div className="flex justify-between items-start mb-4">
                 <h3 className="text-custom-black text-xl">{title}</h3>
                 <Icon className={color} size={24}></Icon>
             </div>
             <div className="text-3xl font-bold pt-6">{mainValue}</div>
-            <div className="text-custom-gray text-xl pt-2"><span className={color}>{secondValue}</span>{subText}</div>
+            <div className="text-custom-gray text-xl pt-2"><span className={`${color} font-bold`}>{secondValue}</span>{subText}<span className={`${color} font-bold`}>{thirdValue}</span>{secondSubText}</div>
         </div>
     );
 };
--- src/pages/DashboardHomePage.jsx
+++ src/pages/DashboardHomePage.jsx
@@ -1,15 +1,16 @@
-import { Boxes, Clock, PhilippinePeso } from "lucide-react";
+import { Boxes, Clock, TrendingUpDownIcon, TriangleAlert } from "lucide-react";
 import StatusCard from "../components/general_components/StatusCard";
 
 const DashboardHome = () => {
   return (
     <div className="animate-fade-in">
-      <h1 className="text-[32px] font-bold text-gray-900 mb-2 leading-none tracking-tight">
+      <h1 className="text-[32px] font-bold text-custom-black mb-2 leading-none tracking-tight">
         Dashboard
       </h1>
-      <p className="text-gray-500 text-sm mb-8">System overview and quick metrics.</p>
+      <p className="text-custom-gray text-sm mb-8">System overview and quick metrics.</p>
       
-      {/* This is where future charts and graphs will go */}
+      {/* ADD AN IF STATEMENT TO CHECK IF MANAGER OR NOT? */}
+
       <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6">
         <StatusCard
         title={"Total Inventory"}
@@ -22,28 +23,36 @@ const DashboardHome = () => {
         />
         <StatusCard 
           title={"Total Revenue"}
-          mainValue={"5"}
-          Icon={PhilippinePeso}
+          mainValue={"₱"+"420.69K"}
+          subText={"from last month"}
+          Icon={TrendingUpDownIcon}
           color={"text-custom-green"}
+          secondValue={"5% "}
         />
 
         <StatusCard 
           title={"Pending Requests"}
           mainValue={"5"}
+          subText="outbound "
           Icon={Clock}
           color={"text-custom-blue"}
+          secondValue={"3 "}
+          thirdValue={"2 "}
+          secondSubText={"inbound"}
+
         />
 
         <StatusCard 
-          title={"Pending Requests"}
-          mainValue={"5"}
-          Icon={Clock}
-          color={"text-custom-blue"}
+          title={"Low Stock Perfumes"}
+          mainValue={"3"}
+          Icon={TriangleAlert}
+          color={"text-custom-yellow"}
+          secondValue={"Requires attention"}
         />
 
       </div>
   
-      <div className="h-64 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center text-gray-400 bg-white">
+      <div className="h-64 mt-8 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center text-gray-400 bg-white">
         Metrics Dashboard Placeholder
         
       </div>
