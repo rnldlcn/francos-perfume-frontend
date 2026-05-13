7e70732bc9445322113d389b48275c2d8ca752c4
cheeseball <cheeseballbusinessemail@gmail.com>
2026-05-13 14:50:16 +0800

    [CHANGE] Adjusted InventoryService merge conflicts

--- src/services/InventoryService.js
+++ src/services/InventoryService.js
@@ -1,23 +1,15 @@
 const API_URL = "http://localhost:5000/api"
 
+
 export const fetchAllInventory = async (token) => {
-<<<<<<< HEAD
-    const response = await fetch(`${API_URL}/inventory/displayAll`, {
-=======
     // 🔧 FIXED: Added ?pageSize=500 to pull all inventory records instantly
     const response = await fetch(`${API_URL}/Inventory/displayAll?pageSize=500`, {
->>>>>>> 90de722d2beaa1d92b9d8f14238328e850ec6a97
         method: 'GET',
         headers: { 
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json' 
         }
     });
-<<<<<<< HEAD
-=======
-    
-    //console.log("API Response:", response);
->>>>>>> 90de722d2beaa1d92b9d8f14238328e850ec6a97
     if (!response.ok) throw new Error(await response.text());
 
     return await response.json();
@@ -31,24 +23,13 @@ export const updateQuantity = async (itemId, newQuantity, token) => {
         body: JSON.stringify({ quantity: newQuantity })
     });
 
-<<<<<<< HEAD
+    
+    //console.log("API Response:", response);
     if (!response.ok) throw new Error(await response.text());
 
     return await response.json();
 }
 
-export const updateStock = async (restockType) => {
-    const response = await fetch(`${API_URL}/Inventory/updateStock`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ restockType })
-    });
+export const updateStock = async () => {
 
-    if (!response.ok) throw new Error(await response.text());
-
-    return await response.json();
 }
-
-=======
-}
->>>>>>> 90de722d2beaa1d92b9d8f14238328e850ec6a97
